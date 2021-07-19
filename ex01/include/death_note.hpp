f3d91aa32e5568f6d8af4926e50fbf2c596ae242
Denisse Madeleine <dmadelei@oa-e5.msk.21-school.ru>
2021-07-19 22:13:08 +0300

    commit

--- ex01/include/death_note.hpp
+++ ex01/include/death_note.hpp
@@ -4,6 +4,7 @@
 class persen_in_death_note
 {
 	public:
+		int			index;
 		std::string first_name;
 		std::string last_name;
 		std::string nickname;
@@ -15,14 +16,33 @@ class persen_in_death_note
 
 }
 
-void print_death_note()
+void print_death_note(persen_in_death_note &persen)
 {
+	int i;
+	int j;
 
+	i = 0;
+	j = 0;
+	while(i < 8)
+	{
+
+		std::cout << /*persen->index*/ | /*persen->first_name*/ | /*persen->last_name*/ | /*persen->nicname*/ << endl;
+		i++;
+	}
 }
 
-void add_persen_in_death_note()
+void add_persen_in_death_note(persen_in_death_note &persen)
 {
-
+	std::cout << "Enter first name" << endl;
+	// std::cin >> persen->first_name;
+	std::cout << "Enter last name" << endl;
+	// std::cin >> 
+	std::cout << "Enter nickname" << endl;
+	// std::cin >>
+	std::cout << "Enter phone number" << endl;
+	// std::cin >>
+	std::cout << "Enter darkest secret" << endl;
+	// std::cin >>
 }
 
 #endif
