0ef6bc4a5e2a8b6ab5f343504acd102d22e46221
Denisse Madeleine <dmadelei@oa-e5.msk.21-school.ru>
2021-07-19 21:40:29 +0300

    create ex00 and Makefile

---
+++ ex01/death_note.cpp
@@ -0,0 +1,27 @@
+#include "death_note.hpp"
+#include <iostream>
+
+int main(void)
+{
+	int i;
+	std::string reading;
+	persen_in_death_note persen[8];
+	
+	while (1!= 0)
+	{
+		std::cout << "When a command has been correctly executed, the program waits for another ADD or SEARCH command until an EXIT command." << std::endln;
+		std::cin >> reading;
+		if (reading == "ADD")
+		{
+	
+		}
+		else if (reading == "SEARCH")
+		{
+		}
+		else if (reading == "END")
+		{
+			return (0);
+		}
+	}
+	return (0);
+}
---
+++ ex01/include/death_note.hpp
@@ -0,0 +1,28 @@
+#ifndef DEATH_NOTE_HPP
+# define DEATH_NOTE_HPP
+
+class persen_in_death_note
+{
+	public:
+		std::string first_name;
+		std::string last_name;
+		std::string nickname;
+		std::string phone_number;
+		std::string darkest_secret;
+		persen_in_death_note(void);
+		~persen_in_death_note(void);
+	private:
+
+}
+
+void print_death_note()
+{
+
+}
+
+void add_persen_in_death_note()
+{
+
+}
+
+#endif
