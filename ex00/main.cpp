3e08f038bdc114e4b69f1cd96f55ee887b1fe96f
Denisse Madeleine <dmadelei@mi-b3.msk.21-school.ru>
2021-09-02 22:32:02 +0300

    dey 07

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,15 +1,20 @@
-#include "Type_conversion.hpp"
+#include "whaterever.hpp"
 #include <iostream>
 
-int main(int argc, char** argv)
+int main(void)
 {
-	if (argc == 2)
-	{
-		Type_conversion type(argv[1]);
-	}
-	else
-	{
-		std::cout << "Invalid number of arguments." << std::endl;
-	}
-	return (0);
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
 }
