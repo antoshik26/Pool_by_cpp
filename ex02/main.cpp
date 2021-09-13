0a36ab4c85e4896e5c1fae076608798ae8eaa595
Denisse Madeleine <dmadelei@mi-e1.msk.21-school.ru>
2021-09-13 17:42:59 +0300

    ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,9 +2,18 @@
 
 int main()
 {
-	unsigned int n = 5;
-	Array<int> a(n);
-	a[1] = 20;
-	std::cout << a[0] << std::endl;
+	try
+	{
+		unsigned int n = 5;
+		Array<int> a(n);
+		a[1] = 20;
+		std::cout << a[0] << std::endl;
+		std::cout << a[1] << std::endl;
+		std::cout << a[20] << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() <<std::endl;
+	}
 	return (0);
 }
